6908d7f4bb8661a7a0606a7f7a27db9d69edfe1d
aloknarahari <aloknarahari@Aloks-MacBook-Pro.local>
2016-04-27 11:54:35 -0700

    Added Jquery hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-$("#sidebar-button").click(function(){
+$(".button").click(function(){
     if ($(".navigation").hasClass("sidebar-active")) {
           $(".navigation").removeClass("sidebar-active");
         } else {
@@ -24,6 +24,56 @@ scrollTop: $( $.attr(this, 'href')).offset().top
 $('.expand').click(function(){
 	$('.bio-descrip').removeClass("bio-descrip-hidden");
 });
+
+(function(window, document, $, undefined) {
+  "use strict";
+
+  $(function() {
+      var animOptions = {
+          duration: 250,
+          easing: "ease-in-out"
+      };
+
+      var open = false;
+
+      $(".js-toggle").on("click", function() {
+          var $el = $(this);
+          var $rects = $el.find("svg rect");
+
+          if ($el.hasClass("active")) {
+              $rects.eq(1).velocity("stop").velocity("reverse");
+              $rects.eq(0).add($rects.eq(2)).velocity("stop").velocity("reverse");
+          } else {
+              $rects.eq(1).velocity({
+                  opacity: 0
+              }, animOptions);
+
+              $rects.eq(0).velocity({
+                  transformOrigin: [30, 25],
+                  translateY: 17,
+                  rotateZ: 45
+              }, animOptions);
+
+              $rects.eq(2).velocity({
+                  transformOrigin: [30, 25],
+                  translateY: -17,
+                  rotateZ: -45
+              }, animOptions);
+          }
+
+          $el.toggleClass("active");
+      });
+  });
+
+})(window, document, jQuery);
+
+
+
+
+
+
+
+
 
 
 
